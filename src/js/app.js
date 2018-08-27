4a5e8f049aed641cbc54767fa80200a2cb63ac5b
salaroli <fernando@salaro.li>
2018-08-26 21:30:09 -0300

    mobile pronto

--- src/js/app.js
+++ src/js/app.js
@@ -2,3 +2,14 @@
 
 // Say hello
 console.log("🦊 Hello! Edit me in src/js/app.js");
+
+$(".dehaze").click(function() {
+    $(".menu").addClass("menu--active");
+});
+
+$(".menu__img").click(function() {
+    $(".menu").removeClass("menu--active");
+});
+$(".menu li").click(function() {
+    $(".menu").removeClass("menu--active");
+});
